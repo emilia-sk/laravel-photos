3e3940f6165b82fedb0024c38319ef3856d3064d
Emilia <emilia66x@gmail.com>
2023-06-01 15:33:01 +0300

    Create main page (user part)

---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,24 @@
+<div class="container-fluid p-0">
+    <header class="p-3 text-bg-dark">
+        <div class="container">
+            <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
+                <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
+                    <li><a href="#" class="nav-link px-2 text-white">Начало</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Снимки</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Потребители</a></li>
+                    <li><a href="#" class="nav-link px-2 text-white">Контакти</a></li>
+                </ul>
+                <div class="text-end">
+                    <!--USER NOT LOGGED IN-->
+                    <button type="button" class="btn btn-outline-light me-2">Вход</button>
+                    <button type="button" class="btn btn-warning">Регистрация</button>
+
+                    <!--USER LOGGED IN-->
+                    <p class="text-welcome d-inline pb-3 px-2"> Здравей, ...</p>
+                    <button type="button" class="btn btn-outline-light me-2">Изход</button>
+                    <button type="button" class="btn btn-outline-light me-2">Качи Снимка</button>
+                </div>
+            </div>
+        </div>
+    </header>
+</div>
---
+++ resources/views/index.blade.php
@@ -0,0 +1,23 @@
+<x-header/>
+<x-layout/>
+
+<div class="album py-5">
+    <div class="container">
+
+      <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+
+        <div class="col"><!--PHOTO START-->
+          <div class="card shadow-sm">
+            <img class="bd-placeholder-img card-img-top" src="{{asset('images/no-image.png')}}">
+            <div class="card-body d-flex justify-content-between align-items-center">
+              <p class="card-text">Описание</p>
+                  <a href="" class="btn btn-sm btn-outline-secondary">Преглед</a>
+              </div>
+            </div>
+          </div>
+        </div><!--PHOTO END-->
+
+      
+      </div>
+    </div>
+  </div>
