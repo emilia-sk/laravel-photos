714613b616894ca27905f8852192dbf63d26e425
Emilia <emilia66x@gmail.com>
2023-06-01 16:00:01 +0300

    Migrated DB and Seeded DB

---
+++ database/factories/PhotoFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Model>
+ */
+class PhotoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->name(),
+            'description' => $this->faker->sentence(2),
+            'photo' => $this->faker->imageUrl(),
+            'author'=> $this->faker->name(),
+        ];
+    }
+}
