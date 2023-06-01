ce7671eb143d2de195aa707b59a0f17e586bcfef
Emilia <emilia66x@gmail.com>
2023-06-01 22:28:17 +0300

    Add pagination

--- app/Http/Controllers/PhotosController.php
+++ app/Http/Controllers/PhotosController.php
@@ -6,19 +6,53 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Photo;
 
+
 class PhotosController extends Controller
 {
-    //Show all photos
-    public function index(){
-        return view('index', [
-            'photos' => Photo::all()
-        ]);
-    }
-    //Show single photo
-    public function show(Photo $photo)
+
+    //Show Index(main) Page
+    public function index()
     {
-        return view('photo-single', [
-            'photo' => $photo
+        return view('index');
+    }
+
+    //Show all photos
+    public function allPhotos()
+    {
+        return view('photo-all', [
+            'photos' => Photo::latest()->paginate(10)
         ]);
     }
+
+    //Show upload photo page
+    public function upload()
+    {
+        return view('photo-upload');
+    }
+
+    //Store photo data 
+    public function store(Request $request)
+    {
+        $formFields = $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+            'photo' => 'required',
+        ]);
+
+        if ($request->hasFile('photo')) {
+            $formFields['photo'] = $request->file('photo')->store('photos', 'public');
+        }
+
+        Photo::create($formFields);
+
+        return redirect('photo-all');
+    }
+
+       //Show single photo
+       public function show(Photo $photo)
+       {
+           return view('photo-single', [
+               'photo' => $photo
+           ]);
+       }
 }
