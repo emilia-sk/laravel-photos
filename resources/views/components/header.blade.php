a6ee10a948dbecb80d36f2470c16f08a05ebb516
Emilia <emilia66x@gmail.com>
2023-06-02 10:57:38 +0300

    Update links

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -4,7 +4,7 @@
             <div class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start">
                 <ul class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0">
                     <li><a href="/" class="nav-link px-2 text-white">Начало</a></li>
-                    <li><a href="photo-all" class="nav-link px-2 text-white">Снимки</a></li>
+                    <li><a href="/photo-all" class="nav-link px-2 text-white">Снимки</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">Потребители</a></li>
                     <li><a href="#" class="nav-link px-2 text-white">Контакти</a></li>
                 </ul>
@@ -12,15 +12,15 @@
                     <!--USER LOGGED IN-->
                     @auth
                         <p class="text-welcome d-inline pb-3 px-2"> Здравей, {{ auth()->user()->name }}</p>
-                        <a href="photo-upload" class="btn btn-outline-light me-2">Качи Снимка</a>
+                        <a href="/photo-upload" class="btn btn-outline-light me-2">Качи Снимка</a>
                         <form method="POST" action="/logout" class="d-inline">
                             @csrf
                             <button type="submit" class="btn btn-success me-2">Изход</button>
                         </form>
                     @else
                         <!--USER NOT LOGGED IN-->
-                        <a href="login" class="btn btn-outline-light me-2">Вход</a>
-                        <a href="register" class="btn btn-success">Регистрация</a>
+                        <a href="/login" class="btn btn-outline-light me-2">Вход</a>
+                        <a href="/register" class="btn btn-success">Регистрация</a>
                     @endauth
                 </div>
             </div>
